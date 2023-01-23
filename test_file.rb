3850bcf3665d290d8f31e0dc45949dc6265e00d7
Ying Zhou <yinglinzhou0@gmail.com>
2023-01-23 17:14:43 -0500

    my_select

--- test_file.rb
+++ test_file.rb
@@ -3,17 +3,23 @@ class Array
     def my_each(&prc)
         i = 0
         while i < self.length
-            puts prc.call(self[i])
+            prc.call(self[i])
             i += 1
         end
-
         return self
-
     end
 
+    def my_select(&prc)
+        finalarr = []
+
+        i = 0
+        while i < self.length
+            finalarr << self[i] if self.my_each(&prc)
+            i += 1
+        end
+        finalarr
+    end
     
-
-
 
 end
 
